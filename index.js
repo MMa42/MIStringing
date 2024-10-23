863946ab9245fc0d292afab780703d063ea86705
MMa42 <mmar25college@gmail.com>
2024-10-23 16:14:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 document.getElementById("schedule-button").addEventListener("click", function() {
     window.open("https://calendly.com/mmar25college/30min", "_blank");
   });
-  
+ document.getElementById.("schedule-button").addEventListener('touchstart', function() {
+   window.open("https://calendly.com/mmar25college/30min", "_blank");
+});
 
