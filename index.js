bb52c6298ecd79abb1c3c097077842006cf719c8
MMa42 <mmar25college@gmail.com>
2024-10-23 19:32:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-document.getElementById("schedule-button").addEventListener("click", function() {
-    window.open("https://calendly.com/mmar25college/30min", "_blank");
-  });
-document.getElementById.("schedule-button").addEventListener('touchstart', function() {
-   window.open("https://calendly.com/mmar25college/30min", "_blank");
-});
-
+document.getElementById('schedule-button').onclick = function() {
+    // Perform JavaScript redirection, without preventing the default action
+    window.location.href = "https://calendly.com/mmar25college/30min";
+  };
